4dca8deea20e9e9eb8e5aca07e4214fcee530621
Andrés Cardona <andres.cardo98@gmail.com>
2023-10-03 23:52:51 -0500

    Realizar conexión Java y SQL Server

--- src/main/java/co/edu/uco/tiendaonline/data/dao/daofactory/concrete/SQLServerDAOFactory.java
+++ src/main/java/co/edu/uco/tiendaonline/data/dao/daofactory/concrete/SQLServerDAOFactory.java
@@ -1,6 +1,8 @@
 package co.edu.uco.tiendaonline.data.dao.daofactory.concrete;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import co.edu.uco.tiendaonline.data.dao.ClienteDAO;
 import co.edu.uco.tiendaonline.data.dao.TipoIdentificacionDAO;
@@ -11,6 +13,7 @@ import co.edu.uco.tiendaonline.data.dao.daofactory.DAOFactory;
 public final class SQLServerDAOFactory extends DAOFactory{
 
 	private Connection conexion;
+	private boolean enTransaccion = false;
 	
 	public SQLServerDAOFactory() {
 		abrirConexion();
@@ -18,33 +21,66 @@ public final class SQLServerDAOFactory extends DAOFactory{
 	
 	@Override
 	public final void abrirConexion() {
-		// TODO: Your homework will be to obtain conncection with SQL Server Database
-		conexion=null;
+
+		final String url = "jdbc:sqlserver://localhost:1433;databaseName=databaseDOO";
+		final String usuario = "usersql";
+		final String contrasenia = "+javasql2023";
 		
+		try {
+			conexion = DriverManager.getConnection(url, usuario, contrasenia);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
+	
 	@Override
 	public final void cerrarConexion() {
-		// TODO: Your homework will be to close connection
-		
+		try {
+			if (conexion != null && !conexion.isClosed()) {
+				conexion.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void iniciarTransaccion() {
-		// TODO: Your homework will be to init transaction
-		
+		try {
+			if (!enTransaccion) {
+				conexion.setAutoCommit(false);
+				enTransaccion = true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void confirmarTransaccion() {
-		// TODO: Your homework will be to commit transaction
-		
+		try {
+			if (enTransaccion) {
+				conexion.commit();
+				conexion.setAutoCommit(true);
+				enTransaccion = false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
 	}
 
 	@Override
 	public void cancelarTransaccion() {
-		// TODO: Your homework will be to rollback transaction
-		
+		try {
+			if (enTransaccion) {
+				conexion.rollback();
+				conexion.setAutoCommit(true);
+				enTransaccion = false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
 	}
 
 	@Override
